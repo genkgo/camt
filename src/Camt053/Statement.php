ce5f425eb4991717516868aaa248a4cd48f15bfc
Einenlum <einenlum@vmail.me>
2016-06-08 16:41:22 +0200

    Fix to string

--- src/Camt053/Statement.php
+++ src/Camt053/Statement.php
@@ -8,7 +8,7 @@ namespace Genkgo\Camt\Camt053;
 class Statement
 {
     /**
-     * @var
+     * @var string
      */
     private $id;
     /**
@@ -29,7 +29,7 @@ class Statement
     private $entries = [];
 
     /**
-     * @param $id
+     * @param string $id
      * @param \DateTimeImmutable $createdOn
      * @param Account $account
      */
@@ -41,7 +41,7 @@ class Statement
     }
 
     /**
-     * @return mixed
+     * @return string
      */
     public function getId()
     {
